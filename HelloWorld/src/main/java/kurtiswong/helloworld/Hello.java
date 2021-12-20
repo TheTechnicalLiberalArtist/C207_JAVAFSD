4ce7550c84c8174b56d9d112baeebe5f36ca712a
Kurtis Wong <kurtis.portfolio.se@gmail.com>
2021-12-21 03:26:24 +0800

    Hello World in Netbeans

---
+++ HelloWorld/src/main/java/kurtiswong/helloworld/Hello.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author kurtiswong
+ * email: kurtis.portfolio.se@gmail.com
+ * date: 21/12/21
+ * purpose: My first Hello World program in an IDE
+ */
+package kurtiswong.helloworld;
+public class Hello {
+    public static void main (String[] args) {
+    System.out.println("Hello Kurtis!");
+    }    
+}
