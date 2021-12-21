6b6844187c6c3e2af08ff5f1dabf341feb2dc28f
Kurtis Wong <kurtis.portfolio.se@gmail.com>
2021-12-22 04:51:06 +0800

    Adder file

---
+++ Adder/src/main/java/kurtiswong/adder/Adder.java
@@ -0,0 +1,22 @@
+
+package kurtiswong.adder;
+
+/**
+ * @author kurtiswong
+ * email: kurtis.portfolio.se@gmail.com
+ * date: 22/12/21
+ * purpose: Adder(Hard-coded)
+ */
+public class Adder {
+    public static void main(String[] args) {
+        int sum = 0;
+        int operand1 = 3;
+        int operand2 = 2;
+        
+        
+        sum = operand1 + operand2;
+        
+        System.out.println("Sum is: " + sum);
+    }
+    
+}
--- HelloWorld/src/main/java/kurtiswong/helloworld/Hello.java
+++ HelloWorld/src/main/java/kurtiswong/helloworld/Hello.java
@@ -8,6 +8,7 @@
 package kurtiswong.helloworld;
 public class Hello {
     public static void main (String[] args) {
-    System.out.println("Hello Kurtis!");
+    System.out.println("Hello World!");
+    System.out.println("My name is Kurtis");
     }    
 }
