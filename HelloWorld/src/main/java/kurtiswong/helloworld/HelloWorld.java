64c04acdf74ca62d2707551ecbab454a35467121
Kurtis Wong <kurtis.portfolio.se@gmail.com>
2021-12-22 05:27:31 +0800

    Hello World Program

---
+++ HelloWorld/src/main/java/kurtiswong/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+package kurtiswong.helloworld;
+
+/**
+* @author kurtiswong
+* email: kurtis.portfolio.se@gmail.com
+* date: 21/12/21
+* purpose: First Hello World Program
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
