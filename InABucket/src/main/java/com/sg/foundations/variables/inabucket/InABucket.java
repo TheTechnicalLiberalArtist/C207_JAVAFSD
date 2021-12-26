e69b177fe9c220b595ca72c3c2e1f916524b4daf
Kurtis Wong <kurtis.portfolio.se@gmail.com>
2021-12-27 00:13:57 +0800

    Variable declaration exercise

---
+++ InABucket/src/main/java/com/sg/foundations/variables/inabucket/InABucket.java
@@ -0,0 +1,27 @@
+
+package com.sg.foundations.variables.inabucket;
+
+/**
+* @author kurtiswong
+* email: kurtis.portfolio.se@gmail.com
+* date: 27/12/21
+* purpose: In a bucket variable exercise
+ */
+public class InABucket {
+    public static void main (String[] args) {
+        String walrus;
+        double piesEaten;
+        float weightOfTeacupPig;
+        int grainsOfSand;
+        
+        walrus = "Ruby Chu";
+        piesEaten = 33.2;
+        weightOfTeacupPig = (float) 23.44;
+        grainsOfSand = 10;
+        
+        System.out.println("My pet name is: " + walrus);
+        System.out.println("The number of pies I ate: " + piesEaten);
+        System.out.println("The weight of my pet is: " + weightOfTeacupPig);
+        System.out.println("The number of sand: " + grainsOfSand);
+    }
+}
