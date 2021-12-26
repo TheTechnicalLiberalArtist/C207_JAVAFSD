c6f229a05c624b37fadd6b0ba4e10ffded4481db
Kurtis Wong <kurtis.portfolio.se@gmail.com>
2021-12-27 07:28:09 +0800

    Restaurant Menu Exercise

---
+++ MenuOfChampions/src/main/java/com/sg/foundations/variables/menuofchampions/MenuOfChampion.java
@@ -0,0 +1,22 @@
+
+package com.sg.foundations.variables.menuofchampions;
+
+/**
+* @author kurtiswong
+* email: kurtis.portfolio.se@gmail.com
+* date: 27/12/21
+* purpose: Variable Exercise
+ */
+public class MenuOfChampion {
+    public static void main (String[] args){
+        System.out.println("Welcome to my restaurant! \n");
+        
+        float burger = 25.50f;
+        float pasta = 29.90f;
+        float steak = 55.40f;
+        
+        System.out.println("$" + burger + " *** " + "Burger King");
+        System.out.println("$" + pasta + " *** " + "Squid Ink Pasta");
+        System.out.println("$" + burger + " *** " + "Ribeye Wagyu");
+    }
+}
